a3b2fb8c39c5d8bdbf3d5f3036cdf18b50956aa0
Emmanuel Balogun <emmanx25@gmail.com>
2024-04-27 02:31:44 +0100

    Multiples of 3 or 5

---
+++ Kata.py
@@ -0,0 +1,33 @@
+""" If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+Additionally, if the number is negative, return 0.
+
+Note: If the number is a multiple of both 3 and 5, only count it once.
+"""
+# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+def solution(number):
+    if number < 0:
+        return 0
+    else:
+        return sum([i for i in range(number) if i % 3 == 0 or i % 5 == 0])
+
+# OR
+
+def solution(number):
+    # Return 0 for negative numbers
+    if number < 0:
+        return 0
+    
+    sum = 0
+    
+    for i in range(number):
+        # Check if the number is a multiple of 3 or 5
+        if i % 3 == 0 or i % 5 == 0:
+            sum += i  # Add the multiple itself to the sum
+            
+    return sum
+
+# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
