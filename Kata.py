eaf3e2a5e484fc001a709d0299ee04bd35bc6071
Emmanuel Balogun <emmanx25@gmail.com>
2024-04-27 02:53:39 +0100

    Counting sheep...

--- Kata.py
+++ Kata.py
@@ -31,3 +31,29 @@ def solution(number):
     return sum
 
 # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    
+"""
+Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+For example,
+
+[True,  True,  True,  False,
+  True,  True,  True,  True ,
+  True,  False, True,  False,
+  True,  False, False, True ,
+  True,  True,  True,  True ,
+  False, False, True,  True]
+The correct answer would be 17.
+
+Hint: Don't forget to check for bad values like null/undefined
+
+"""
+# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    
+def count_sheeps(sheep):
+    return sheep.count(True)
+
+# OR
+
+def count_sheeps(sheep):
+    return sum(sheep)
