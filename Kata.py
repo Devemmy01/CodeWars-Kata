24f8c021eae78db33700361fdd1f87f7c8f58539
Emmanuel Balogun <emmanx25@gmail.com>
2024-04-28 23:50:23 +0000

    Is n divisible by x and y?

--- Kata.py
+++ Kata.py
@@ -91,3 +91,14 @@ def maskify(cc: str) -> str:
     return '#' * (len(cc) - 4) + cc[-4:]
 
 # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+"""
+Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero numbers.
+"""
+
+# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    
+def is_divisible(n, x, y):
+    return n % x == 0 and n % y == 0
+
+# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
