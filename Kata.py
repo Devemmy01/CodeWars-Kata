2bc9750b06c2176323058badf199d9b2b5c19076
Emmanuel <emmanx25@gmail.com>
2024-04-29 01:59:42 +0300

    Update Kata.py

--- Kata.py
+++ Kata.py
@@ -22,6 +22,13 @@ def solution(number):
         return 0
     
     sum = 0
+
+
+    """
+Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+Your task is to write a function maskify, which changes all but the last four characters into '#'.
+"""
     
     for i in range(number):
         # Check if the number is a multiple of 3 or 5
