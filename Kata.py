bfe4bca31df0302f0140793b02fa0de94745fb30
Emmanuel <emmanx25@gmail.com>
2024-04-28 12:00:00 +0000

    Update Kata.py

--- Kata.py
+++ Kata.py
@@ -1,9 +1,6 @@
 """ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-
 Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
-
 Additionally, if the number is negative, return 0.
-
 Note: If the number is a multiple of both 3 and 5, only count it once.
 """
 # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
@@ -34,9 +31,7 @@ def solution(number):
     
 """
 Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
-
 For example,
-
 [True,  True,  True,  False,
   True,  True,  True,  True ,
   True,  False, True,  False,
@@ -46,7 +41,6 @@ For example,
 The correct answer would be 17.
 
 Hint: Don't forget to check for bad values like null/undefined
-
 """
 # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
     
