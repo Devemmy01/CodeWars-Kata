09ada7501c6c146b436fb2adf9b37878d882b21f
Emmanuel Balogun <emmanx25@gmail.com>
2024-05-01 23:57:56 +0100

    Detect Pangram

--- Kata.py
+++ Kata.py
@@ -158,3 +158,21 @@ def greet():
 
 # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
     
+"""
+A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+
+Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+"""
+
+# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    
+def is_pangram(s):
+    alphabets = ('abcdefghijklmnopqrstuvwxyz')
+    s_lower = s.lower()
+    for i in alphabets:
+        if i not in s_lower:
+            return False
+    return True
+
+# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    
