6e01302226752fc60ca5ca9f20fc33aa52830863
Emmanuel Balogun <emmanx25@gmail.com>
2024-05-02 00:01:29 +0000

    Bit Counting

--- Kata.py
+++ Kata.py
@@ -218,3 +218,12 @@ Write a function that takes an integer as input, and returns the number of bits 
 
 Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
 """
+
+# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    
+def count_bits(n):
+    return bin(n).count('1')
+
+# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    
+    
