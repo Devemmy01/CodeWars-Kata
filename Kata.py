f45f44e2d76eec9bc0250ec5559309fa1afe4774
Emmanuel Balogun <emmanx25@gmail.com>
2024-05-03 23:58:07 +0100

    Sum of odd numbers

--- Kata.py
+++ Kata.py
@@ -289,5 +289,37 @@ def solution(text, ending):
 # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
     
 """
+Given the triangle of consecutive odd numbers:
 
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+...
+Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+1 -->  1
+2 --> 3 + 5 = 8
 """
+
+# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    
+def row_sum_odd_numbers(n):
+    # Calculate the first number of the nth row
+    first_number = n * (n - 1) +1
+    
+    row_sum = 0
+    
+    for i in range(n):
+        row_sum += first_number + 2 * i
+
+    return row_sum
+
+# OR
+
+def row_sum_odd_numbers(n):
+    return n ** 3
+
+# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    
