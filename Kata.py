c145ecd1b7398910bf684392c261b68be9c49840
Emmanuel Balogun <emmanx25@gmail.com>
2024-05-03 23:44:59 +0100

    String ends with?

--- Kata.py
+++ Kata.py
@@ -269,3 +269,25 @@ def is_isogram(string):
     for letter in string:
         if string.count(letter) > 1: return False
     return True
+
+# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    
+"""
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') # returns true
+solution('abc', 'd') # returns false
+"""
+
+# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    
+def solution(text, ending):
+    return text.endswith(ending)
+
+# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    
+"""
+
+"""
