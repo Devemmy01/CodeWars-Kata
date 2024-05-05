4d0880014ff1f1e32a41f0b0373fc24e31277d32
Emmanuel Balogun <emmanx25@gmail.com>
2024-05-05 23:57:53 +0100

    Shortest Word

--- Kata.py
+++ Kata.py
@@ -409,3 +409,16 @@ def cockroach_speed(s):
 
 # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
     
+"""
+Simple, given a string of words, return the length of the shortest word(s).
+String will never be empty and you do not need to account for different data types.
+"""
+
+# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    
+def find_short(s):
+    short = s.split()
+    return min(len(word) for word in short)
+
+# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    
