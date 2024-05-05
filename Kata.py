9d6bf6b61ecae6850b13fc4dca55ab4cb9106ef4
Emmanuel Balogun <emmanx25@gmail.com>
2024-05-06 00:28:25 +0100

    Abbreviate a Two Word Name

--- Kata.py
+++ Kata.py
@@ -457,3 +457,21 @@ def find_average(numbers):
     if not numbers:
         return 0
     return sum(numbers) / len(numbers)
+
+# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    
+"""
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+The output should be two capital letters with a dot separating them.
+It should look like this:
+Sam Harris => S.H
+patrick feeney => P.F
+"""
+
+# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    
+def abbrev_name(name):
+    words = name.split()
+    cap_first_letters = [i[0].upper() for i in words]
+    initial = '.'.join(cap_first_letters)
+    return initial
