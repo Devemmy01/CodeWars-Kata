dc29863fb1b0daefdcb9d14dfab04e0088a53df0
Emmanuel Balogun <emmanx25@gmail.com>
2024-05-11 00:51:56 +0100

    Reversed Words

--- Kata.py
+++ Kata.py
@@ -491,3 +491,22 @@ def grow(arr):
     for i in arr:
         result *= i
     return result
+
+# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+"""
+Complete the solution so that it reverses all of the words within the string passed in.
+
+Words are separated by exactly one space and there are no leading or trailing spaces.
+
+Example(Input --> Output):
+
+'The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The'
+"""
+
+# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    
+def reverse_words(s):
+    words = s.split()
+    words.reverse()
+    return " ".join(words)
