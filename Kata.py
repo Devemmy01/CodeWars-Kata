8a2a489b9349ac9601b56cbc46e9b579e983cff8
Emmanuel Balogun <emmanx25@gmail.com>
2024-05-11 01:16:55 +0100

    Opposite number

--- Kata.py
+++ Kata.py
@@ -533,4 +533,15 @@ def count_by(x, n):
         multiples.append(x * i)
     return multiples
     
-    
+"""
+Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+"""
+
+def opposite(number):
+    return -number
