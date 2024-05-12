1356031f9745a4515d858ebc7231debd7ff76e1c
Emmanuel Balogun <emmanx25@gmail.com>
2024-05-12 01:45:18 +0100

    Array plus array

--- Kata.py
+++ Kata.py
@@ -545,3 +545,13 @@ Examples:
 
 def opposite(number):
     return -number
+
+"""
+I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help.
+
+P.S. Each array includes only integer numbers. Output is a number too.
+"""
+
+def array_plus_array(arr1,arr2):
+    arr1.extend(arr2)
+    return sum(arr1)
