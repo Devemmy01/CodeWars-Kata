04b91d7d9bc4d8ebe0a6e340d6e45dcdca239fe2
Emmanuel Balogun <emmanx25@gmail.com>
2024-05-16 02:53:47 +0100

    Quarter of the year

--- kata.js
+++ kata.js
@@ -603,3 +603,23 @@ function evenOrOdd(number) {
 function evenOrOdd(number) {
   return number % 2 === 0 ? 'Even' : 'Odd';
 } 
+
+/*
+Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+
+Constraint:
+
+1 <= month <= 12
+*/
+
+const quarterOf = (month) => {
+  if (month < 1 || month > 12) {
+   throw new Error("Invalid month. Month must be between 1 and 12.");
+ }
+
+ const quarter = Math.ceil(month / 3);
+ return quarter;
+}
+
