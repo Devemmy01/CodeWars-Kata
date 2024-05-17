46ba0586bca9e91c632e0a8e1a9e0cf98d41cbbc
Emmanuel Balogun <emmanx25@gmail.com>
2024-05-17 02:07:31 +0100

    Sum of the first nth term of Series

--- kata.js
+++ kata.js
@@ -660,3 +660,84 @@ time = 11.8--> litres = 5
 function litres(time) {
   return Math.floor(time * 0.5);
 }
+
+/*
+Your task is to write a function which returns the n-th term of the following series, which is the sum of the first n terms of the sequence (n is the input parameter).
+
+S
+e
+r
+i
+e
+s
+:
+1
++
+1
+4
++
+1
+7
++
+1
+10
++
+1
+13
++
+1
+16
++
+…
+Series:1+ 
+4
+1
+​
+ + 
+7
+1
+​
+ + 
+10
+1
+​
+ + 
+13
+1
+​
+ + 
+16
+1
+​
+ +…
+You will need to figure out the rule of the series to complete this.
+
+Rules
+You need to round the answer to 2 decimal places and return it as String.
+
+If the given value is 0 then it should return "0.00".
+
+You will only be given Natural Numbers as arguments.
+
+Examples (Input --> Output)
+n
+1 --> 1 --> "1.00"
+2 --> 1 + 1/4 --> "1.25"
+5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+*/
+
+function SeriesSum(n) {
+  if (n === 0) {
+    return "0.00";
+  }
+
+  let sum = 1;
+  let denominator = 4;
+
+  for (let i = 1; i < n; i++) {
+    sum += 1 / denominator;
+    denominator += 3;
+  }
+
+  return sum.toFixed(2);
+}
