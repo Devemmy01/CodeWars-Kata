653d7bc5586ed96b15173e6231b776b29e2e3bef
Emmanuel Balogun <emmanx25@gmail.com>
2024-05-18 02:43:44 +0100

    Count of positives / sum of negatives

--- Kata.py
+++ Kata.py
@@ -771,3 +771,28 @@ def series_sum(n):
 
 def series_sum(n):
     return '{:.2f}'.format(sum(1.0/(3 * i + 1) for i in range(n)))
+
+"""
+Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+If the input is an empty array or is null, return an empty array.
+
+Example
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+"""
+
+def count_positives_sum_negatives(arr):
+    if not arr:
+        return []
+
+    positive_count = 0
+    negative_sum = 0
+    for num in arr:
+        if num > 0:
+            positive_count += 1
+        elif num < 0:
+            negative_sum += num
+
+    return [positive_count, negative_sum]
