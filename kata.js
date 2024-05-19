b733c1d7b81b2eca5fa0e158723f3a92ca6aa715
Emmanuel Balogun <emmanx25@gmail.com>
2024-05-19 02:45:46 +0100

    Count of positives / sum of negatives

--- kata.js
+++ kata.js
@@ -751,3 +751,33 @@ function SeriesSum(n) {
   
   return s.toFixed(2)
 }
+
+/*
+Given an array of integers.
+
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+If the input is an empty array or is null, return an empty array.
+
+Example
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+*/
+
+function countPositivesSumNegatives(arr) {
+  if (!arr || arr.length === 0) {
+    return [];
+  }
+
+  let positiveCount = 0;
+  let negativeSum = 0;
+
+  for (const num of arr) {
+    if (num > 0) {
+      positiveCount++;
+    } else if (num < 0) {
+      negativeSum += num;
+    }
+  }
+
+  return [positiveCount, negativeSum];
+}
